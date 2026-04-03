86cd9ab4dabdf1304aff6bdbac50455edf983dd3
sampgreenwell-cyber <sam.p.greenwell@gmail.com>
2026-04-02 20:25:58 -0500

    Fix UserModel imports correctly

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import { User, IUser } from '../models/UserModelModel';
+import { User, IUser } from '../models/UserModel';
 
 export interface AuthRequest extends Request {
   user?: IUser;
